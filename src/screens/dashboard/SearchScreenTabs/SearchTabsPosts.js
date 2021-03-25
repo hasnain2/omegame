420b1df4900c1162d2bb86bd032a6f10c86d927b
hasnain67 <muhammadhasnaincp@gmail.com>
2021-03-25 16:07:39 +0500

    new changes pushed

--- src/screens/dashboard/SearchScreenTabs/SearchTabsPosts.js
+++ src/screens/dashboard/SearchScreenTabs/SearchTabsPosts.js
@@ -1,66 +1,81 @@
-
-
-import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
-import { useSelector } from 'react-redux';
-import { AppPostsListings } from '../../../components';
-import { AppTheme } from '../../../config';
-import { GetExplorePosts } from '../../../services';
-import { RemoveDuplicateObjectsFromArray } from '../../../utils/AppHelperMethods';
+import React, {useEffect, useState} from 'react';
+import {View} from 'react-native';
+import {useSelector} from 'react-redux';
+import {AppPostsListings} from '../../../components';
+import {AppTheme} from '../../../config';
+import {GetExplorePosts} from '../../../services';
+import {RemoveDuplicateObjectsFromArray} from '../../../utils/AppHelperMethods';
 
 let enableReset = true;
 let cursorArrPosts = [];
-const SearchTabsPosts = ({ navigation }) => {
-    const { query } = useSelector(state => state.root)
-    let [state, setState] = useState({
-        loading: false,
-        loadingPosts: true, refreshingPosts: false,
-        allPosts: [],
-    });
+const SearchTabsPosts = ({navigation}) => {
+  const {query} = useSelector((state) => state.root);
+  let [state, setState] = useState({
+    loading: false,
+    loadingPosts: true,
+    refreshingPosts: false,
+    allPosts: [],
+  });
 
-    function getexplorepostshelper(cursor, searchQuery) {
-        if (!cursor)
-            cursorArrPosts = []
+  function getexplorepostshelper(cursor, searchQuery) {
+    if (!cursor) cursorArrPosts = [];
 
-        if (!cursorArrPosts.includes(cursor)) {
-            GetExplorePosts((postResponse) => {
-                if (postResponse && (postResponse.length || enableReset)) {
-                    let tempData = postResponse
-                    setState(prev => ({ ...prev, loadingPosts: false, refreshingPosts: false, loading: false, allPosts: RemoveDuplicateObjectsFromArray(tempData) }))
-                } else {
-                    setState(prev => ({ ...prev, loadingPosts: false, refreshingPosts: false, loading: false }))
-                }
-            }, cursor, searchQuery);
-            cursorArrPosts.push(cursor)
-        } else {
-            setState(prev => ({ ...prev, refreshingPosts: false }))
-        }
+    if (!cursorArrPosts.includes(cursor)) {
+      GetExplorePosts(
+        (postResponse) => {
+          if (postResponse && (postResponse.length || enableReset)) {
+            let tempData = postResponse;
+            setState((prev) => ({
+              ...prev,
+              loadingPosts: false,
+              refreshingPosts: false,
+              loading: false,
+              allPosts: RemoveDuplicateObjectsFromArray(tempData),
+            }));
+          } else {
+            setState((prev) => ({
+              ...prev,
+              loadingPosts: false,
+              refreshingPosts: false,
+              loading: false,
+            }));
+          }
+        },
+        cursor,
+        searchQuery,
+      );
+      cursorArrPosts.push(cursor);
+    } else {
+      setState((prev) => ({...prev, refreshingPosts: false}));
     }
+  }
 
-    useEffect(() => {
-        enableReset = true;
-        cursorArrPosts = [];
-        getexplorepostshelper(false, query)
-    }, [query])
-    return (
-        <View style={{ flex: 1 }}>
-            <AppPostsListings navigation={navigation}
-                data={state.allPosts}
-                loading={state.loadingPosts}
-                refreshing={state.refreshingPosts}
-                loadMore={(cursor, refreshControl) => {
-                    enableReset = false;
-                    if (refreshControl) {
-                        setState(prev => ({ ...prev, refreshingPosts: true }));
-                        getexplorepostshelper(false, query)
-                    } else {
-                        getexplorepostshelper(cursor, query)
-                    };
-                }}
-                style={{ backgroundColor: AppTheme.colors.background }} />
-        </View>
-    );
+  useEffect(() => {
+    enableReset = true;
+    cursorArrPosts = [];
+    getexplorepostshelper(false, query);
+  }, [query]);
+  return (
+    <View style={{flex: 1}}>
+      <AppPostsListings
+        navigation={navigation}
+        data={state.allPosts}
+        loading={state.loadingPosts}
+        refreshing={state.refreshingPosts}
+        screenType="search"
+        loadMore={(cursor, refreshControl) => {
+          enableReset = false;
+          if (refreshControl) {
+            setState((prev) => ({...prev, refreshingPosts: true}));
+            getexplorepostshelper(false, query);
+          } else {
+            getexplorepostshelper(cursor, query);
+          }
+        }}
+        style={{backgroundColor: AppTheme.colors.background}}
+      />
+    </View>
+  );
 };
 
-
-export { SearchTabsPosts };
+export {SearchTabsPosts};
