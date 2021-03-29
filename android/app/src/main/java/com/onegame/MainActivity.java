016e252f33166133c40b481d8f4ecfcd1b4567e8
CP <shahjahancodingpixel@gmail.com>
2021-03-29 16:44:05 +0500

    icon, splash screen added

--- android/app/src/main/java/com/onegame/MainActivity.java
+++ android/app/src/main/java/com/onegame/MainActivity.java
@@ -1,8 +1,15 @@
 package com.onegame;
-
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this, R.style.SplashStatusBarTheme);
+    super.onCreate(savedInstanceState);
+  }
 
   @Override
   protected String getMainComponentName() {
