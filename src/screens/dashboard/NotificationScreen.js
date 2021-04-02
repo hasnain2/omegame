50b0db78b2f6936db6dea90d19afa387757837b7
hasnain67 <muhammadhasnaincp@gmail.com>
2021-04-02 11:24:52 +0500

    new changes pushed

--- src/screens/dashboard/NotificationScreen.js
+++ src/screens/dashboard/NotificationScreen.js
@@ -4,31 +4,13 @@ import {FlatList, TouchableOpacity, View} from 'react-native';
 import {RFValue} from 'react-native-responsive-fontsize';
 import {useDispatch, useSelector} from 'react-redux';
 import {DEFAULT_REQ} from '../../../assets/images';
-import {
-  AppBackButton,
-  AppButton,
-  AppLoadingView,
-  AppNoDataFound,
-  AppText,
-} from '../../components';
+import {AppBackButton, AppButton, AppLoadingView, AppNoDataFound, AppText} from '../../components';
 import {UserAvatar} from '../../components/UserAvatar';
 import {AppTheme} from '../../config';
 import {setNotifications} from '../../redux/reducers/notificationsSlice';
-import {
-  ActionsOnUsers,
-  GetNotificationHistory,
-  GetPostByCommentID,
-  ReadUpdateNotificationStatus,
-} from '../../services';
-import {
-  FRIEND_STATUSES_ACTIONS,
-  NOTIFICATION_TYPES,
-} from '../../utils/AppConstants';
-import {
-  AppLogger,
-  AppShowToast,
-  RemoveDuplicateObjectsFromArray,
-} from '../../utils/AppHelperMethods';
+import {ActionsOnUsers, GetNotificationHistory, GetPostByCommentID, ReadUpdateNotificationStatus} from '../../services';
+import {FRIEND_STATUSES_ACTIONS, NOTIFICATION_TYPES} from '../../utils/AppConstants';
+import {AppLogger, AppShowToast, RemoveDuplicateObjectsFromArray} from '../../utils/AppHelperMethods';
 import {AntDesign} from '../../utils/AppIcons';
 
 let tempRequests = [];
@@ -44,24 +26,18 @@ const NotificationScreen = ({navigation, route}) => {
   function getnotificationhistoryhelper(cursor) {
     GetNotificationHistory((notificatioHistoryResponse) => {
       if (notificatioHistoryResponse) {
-        tempRequests = notificatioHistoryResponse.filter(
-          (ii) => ii?.portion === 'upper',
-        );
+        tempRequests = notificatioHistoryResponse.filter((ii) => ii?.portion === 'upper');
         let tempArr = [];
         tempRequests.forEach((io) => {
           if (io?.createdBy?.isFollowing) tempArr.push(io.createdBy._id);
         });
         setState((prev) => ({...prev, isFollowingArray: tempArr}));
-        let tempPlanNotifications = notificatioHistoryResponse.filter(
-          (ii) => ii?.portion === 'lower',
-        );
+        let tempPlanNotifications = notificatioHistoryResponse.filter((ii) => ii?.portion === 'lower');
 
         dispatch(
           setNotifications({
             requests: RemoveDuplicateObjectsFromArray(tempRequests),
-            otherNotifications: RemoveDuplicateObjectsFromArray(
-              tempPlanNotifications,
-            ),
+            otherNotifications: RemoveDuplicateObjectsFromArray(tempPlanNotifications),
           }),
         );
         setState((prev) => ({...prev, loading: false}));
@@ -94,9 +70,7 @@ const NotificationScreen = ({navigation, route}) => {
         if (accept) getnotificationhistoryhelper(false);
       },
       userID,
-      accept
-        ? FRIEND_STATUSES_ACTIONS.ACCEPT_FOLLOW_REQUEST
-        : FRIEND_STATUSES_ACTIONS.DENY_FOLLOW_REQUEST,
+      accept ? FRIEND_STATUSES_ACTIONS.ACCEPT_FOLLOW_REQUEST : FRIEND_STATUSES_ACTIONS.DENY_FOLLOW_REQUEST,
     );
     AppShowToast(accept ? 'Request accepted!' : 'Request denied!');
   }
@@ -136,8 +110,7 @@ const NotificationScreen = ({navigation, route}) => {
         });
       }
     } else if (item?.type === NOTIFICATION_TYPES.CHAT) {
-      if (item?.createdBy)
-        navigation.navigate('ChatWindow', {friend: item?.createdBy});
+      if (item?.createdBy) navigation.navigate('ChatWindow', {friend: item?.createdBy});
     }
   }
   useEffect(() => {
@@ -149,168 +122,133 @@ const NotificationScreen = ({navigation, route}) => {
 
       <View style={{padding: RFValue(5), flex: 1}}>
         <View>
-          <AppText
-            size={1}
-            bold={true}
-            style={{paddingVertical: RFValue(10)}}
-            color={AppTheme.colors.lightGrey}>
-            FOLLOW REQUESTS
-          </AppText>
-
-          {!state.loading && notifications?.requests?.length < 1 ? (
-            <AppNoDataFound msg={'No follow requests found!'} />
-          ) : (
-            <FlatList
-              data={notifications?.requests?.slice(0, state.limitRequests)}
-              initialNumToRender={10}
-              windowSize={3}
-              contentContainerStyle={{justifyContent: 'center'}}
-              // removeClippedSubviews={true}
-              maxToRenderPerBatch={10}
-              // bounces={false}
-              keyExtractor={(ii) => (ii?._id || '') + 'you'}
-              renderItem={({item, index}) => {
-                return (
-                  <TouchableOpacity
-                    activeOpacity={0.8}
-                    style={{paddingBottom: RFValue(15)}}
-                    onPress={() => {
-                      handlenotificationclick(item);
-                    }}>
-                    <View
-                      style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        padding: RFValue(10),
-                        borderBottomWidth: 0.5,
-                        borderBottomColor: 'grey',
+          {!state.loading && notifications?.requests?.length < 1 ? null : (
+            <View>
+              <AppText size={1} bold={true} style={{paddingVertical: RFValue(10)}} color={AppTheme.colors.lightGrey}>
+                FOLLOW REQUESTS
+              </AppText>
+              <FlatList
+                data={notifications?.requests?.slice(0, state.limitRequests)}
+                initialNumToRender={10}
+                windowSize={3}
+                contentContainerStyle={{justifyContent: 'center'}}
+                // removeClippedSubviews={true}
+                maxToRenderPerBatch={10}
+                // bounces={false}
+                keyExtractor={(ii) => (ii?._id || '') + 'you'}
+                renderItem={({item, index}) => {
+                  return (
+                    <TouchableOpacity
+                      activeOpacity={0.8}
+                      style={{paddingBottom: RFValue(15)}}
+                      onPress={() => {
+                        handlenotificationclick(item);
                       }}>
-                      <View>
-                        <View>
-                          <UserAvatar
-                            corner={item?.createdBy?.corner || ''}
-                            color={item?.createdBy?.cornerColor}
-                            source={
-                              item?.createdBy?.pic
-                                ? {uri: item?.createdBy?.pic}
-                                : DEFAULT_REQ
-                            }
-                            size={50}
-                          />
-                          <View
-                            style={{
-                              position: 'absolute',
-                              bottom: RFValue(2),
-                              right: RFValue(2),
-                              backgroundColor: 'white',
-                              borderRadius: 90,
-                            }}>
-                            {state.isFollowingArray.includes(
-                              item?.createdBy?._id,
-                            ) ? (
-                              <AntDesign
-                                name={'minus'}
-                                onPress={() => {
-                                  AppLogger(
-                                    '-----------',
-                                    JSON.stringify(item.createdBy),
-                                  );
-                                  IsFollowingHelper(item);
-                                  ActionsOnUsers(
-                                    () => {},
-                                    item?.createdBy?._id,
-                                    FRIEND_STATUSES_ACTIONS.UNFOLLOW,
-                                  );
-                                }}
-                                style={{
-                                  fontSize: RFValue(15),
-                                  padding: RFValue(2),
-                                  color: AppTheme.colors.primary,
-                                }}
-                              />
-                            ) : (
-                              <AntDesign
-                                name={'pluscircle'}
-                                onPress={() => {
-                                  AppLogger(
-                                    '-----------',
-                                    JSON.stringify(item.createdBy),
-                                  );
-                                  IsFollowingHelper(item);
-                                  ActionsOnUsers(
-                                    () => {},
-                                    item?.createdBy?._id,
-                                    FRIEND_STATUSES_ACTIONS.FOLLOW,
-                                  );
-                                }}
-                                style={{
-                                  fontSize: RFValue(15),
-                                  padding: RFValue(2),
-                                  color: AppTheme.colors.primary,
-                                }}
-                              />
-                            )}
-                          </View>
-                        </View>
-                      </View>
                       <View
                         style={{
-                          flex: 1,
-                          paddingLeft: RFValue(10),
+                          flexDirection: 'row',
+                          justifyContent: 'space-between',
+                          padding: RFValue(10),
+                          borderBottomWidth: 0.5,
+                          borderBottomColor: 'grey',
                         }}>
-                        <View style={{flexDirection: 'row'}}>
-                          <AppText size={2} bold="bold">
-                            {item.body.split(' ')[0]}
-                          </AppText>
-                          <AppText size={2} style={{textAlign: 'left'}}>
-                            {' wants to follow you.'}
-                          </AppText>
+                        <View>
+                          <View>
+                            <UserAvatar
+                              corner={item?.createdBy?.corner || ''}
+                              color={item?.createdBy?.cornerColor}
+                              source={item?.createdBy?.pic ? {uri: item?.createdBy?.pic} : DEFAULT_REQ}
+                              size={50}
+                            />
+                            <View
+                              style={{
+                                position: 'absolute',
+                                bottom: RFValue(2),
+                                right: RFValue(2),
+                                backgroundColor: 'white',
+                                borderRadius: 90,
+                              }}>
+                              {state.isFollowingArray.includes(item?.createdBy?._id) ? (
+                                <AntDesign
+                                  name={'minus'}
+                                  onPress={() => {
+                                    AppLogger('-----------', JSON.stringify(item.createdBy));
+                                    IsFollowingHelper(item);
+                                    ActionsOnUsers(() => {}, item?.createdBy?._id, FRIEND_STATUSES_ACTIONS.UNFOLLOW);
+                                  }}
+                                  style={{
+                                    fontSize: RFValue(15),
+                                    padding: RFValue(2),
+                                    color: AppTheme.colors.primary,
+                                  }}
+                                />
+                              ) : (
+                                <AntDesign
+                                  name={'pluscircle'}
+                                  onPress={() => {
+                                    AppLogger('-----------', JSON.stringify(item.createdBy));
+                                    IsFollowingHelper(item);
+                                    ActionsOnUsers(() => {}, item?.createdBy?._id, FRIEND_STATUSES_ACTIONS.FOLLOW);
+                                  }}
+                                  style={{
+                                    fontSize: RFValue(15),
+                                    padding: RFValue(2),
+                                    color: AppTheme.colors.primary,
+                                  }}
+                                />
+                              )}
+                            </View>
+                          </View>
                         </View>
                         <View
                           style={{
-                            justifyContent: 'flex-end',
-                            flexDirection: 'row',
-                            paddingVertical: RFValue(5),
+                            flex: 1,
+                            paddingLeft: RFValue(10),
                           }}>
-                          <View style={{flex: 0.3}} />
-                          <View style={{flex: 0.5}}>
-                            <AppButton
-                              fill={true}
-                              onPress={() => {
-                                if (!item?.read)
-                                  ReadUpdateNotificationStatus(item?._id);
-                                acceptOrDenyRequest(
-                                  item?.createdBy?._id,
-                                  true,
-                                  index,
-                                );
-                              }}
-                              label={'ACCEPT'}
-                              size="small"
-                            />
+                          <View style={{flexDirection: 'row'}}>
+                            <AppText size={2} bold="bold">
+                              {item.body.split(' ')[0]}
+                            </AppText>
+                            <AppText size={2} style={{textAlign: 'left'}}>
+                              {' wants to follow you.'}
+                            </AppText>
                           </View>
-                          <View style={{flex: 0.5, paddingLeft: RFValue(5)}}>
-                            <AppButton
-                              onPress={() => {
-                                if (!item?.read)
-                                  ReadUpdateNotificationStatus(item?._id);
-                                acceptOrDenyRequest(
-                                  item?.createdBy?._id,
-                                  false,
-                                  index,
-                                );
-                              }}
-                              size="small"
-                              label={'DENY'}
-                            />
+                          <View
+                            style={{
+                              justifyContent: 'flex-end',
+                              flexDirection: 'row',
+                              paddingVertical: RFValue(5),
+                            }}>
+                            <View style={{flex: 0.3}} />
+                            <View style={{flex: 0.5}}>
+                              <AppButton
+                                fill={true}
+                                onPress={() => {
+                                  if (!item?.read) ReadUpdateNotificationStatus(item?._id);
+                                  acceptOrDenyRequest(item?.createdBy?._id, true, index);
+                                }}
+                                label={'ACCEPT'}
+                                size="small"
+                              />
+                            </View>
+                            <View style={{flex: 0.5, paddingLeft: RFValue(5)}}>
+                              <AppButton
+                                onPress={() => {
+                                  if (!item?.read) ReadUpdateNotificationStatus(item?._id);
+                                  acceptOrDenyRequest(item?.createdBy?._id, false, index);
+                                }}
+                                size="small"
+                                label={'DENY'}
+                              />
+                            </View>
                           </View>
                         </View>
                       </View>
-                    </View>
-                  </TouchableOpacity>
-                );
-              }}
-            />
+                    </TouchableOpacity>
+                  );
+                }}
+              />
+            </View>
           )}
 
           {notifications?.requests?.length > 2 ? (
@@ -340,11 +278,7 @@ const NotificationScreen = ({navigation, route}) => {
         </View>
 
         <View style={{flex: 1}}>
-          <AppText
-            size={1}
-            bold={true}
-            color={AppTheme.colors.lightGrey}
-            style={{paddingVertical: RFValue(10)}}>
+          <AppText size={1} bold={true} color={AppTheme.colors.lightGrey} style={{paddingVertical: RFValue(10)}}>
             NOTIFICATIONS
           </AppText>
 
@@ -378,17 +312,11 @@ const NotificationScreen = ({navigation, route}) => {
                     <UserAvatar
                       corner={item?.createdBy?.corner || ''}
                       color={item?.createdBy?.cornerColor}
-                      source={
-                        item?.createdBy?.pic
-                          ? {uri: item?.createdBy?.pic}
-                          : DEFAULT_REQ
-                      }
+                      source={item?.createdBy?.pic ? {uri: item?.createdBy?.pic} : DEFAULT_REQ}
                       size={40}
                     />
                     <View style={{flex: 1, paddingHorizontal: RFValue(10)}}>
-                      <AppText
-                        color={item.read ? AppTheme.colors.lightGrey : 'white'}
-                        size={2}>
+                      <AppText color={item.read ? AppTheme.colors.lightGrey : 'white'} size={2}>
                         {item.body}{' '}
                         <AppText color={AppTheme.colors.lightGrey} size={2}>
                           {moment(item.createdAt).fromNow(true)}
